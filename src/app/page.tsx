23dac3c6b285015a7bf58ae2d6fe15a885722d6f
sandro-git14 <lukavarabaloidze@gmail.com>
2026-03-31 13:32:52 +0400

    Added Whole UI And Images !

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,36 @@
-export default function Home() {
+import '../app/styles/page.css'
+
+export default function Home({
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <>
+<div>
+    <br /><br />
     <center>
-        <h1>Hello World!</h1>
+    <div className="HelloPanel">
+      <h1 className='WebHello'>მოგესალმებით</h1>
+    </div>
+
+
+    <div>
+      <img 
+      src="/LcodeLogo.png" 
+      alt="Logo On Center Left" 
+      className='LogoRight'/>
+    </div>
+
+    <div>
+      <img 
+      src="/LcodeLogo.png" 
+      alt="Logo In Center Right" 
+      className='LogoLeft'
+      />
+    </div>
+
     </center>
+</div>
     </>
   );
 }
