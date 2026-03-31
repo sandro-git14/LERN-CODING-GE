c19b0d631ac96f1157c055346a5751e05494efd2
sandro-git14 <lukavarabaloidze@gmail.com>
2026-03-31 14:35:48 +0400

    Added More UI Elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,35 +6,46 @@ export default function Home({
 }) {
   return (
     <>
-<div className='Items'>
-    <br /><br />
-    <center>
-    <div className="HelloPanel">
-      <h1 className='WebHello'>მოგესალმებით მთავარ გვერდზე</h1>
-    </div>
+      <br /><br />
+      <center>
+        <div className="HelloPanel">
+          <h1 className='WebHello'>მოგესალმებით მთავარ გვერდზე</h1>
+        </div>
 
+        <div>
+          <img 
+            src="/LcodeLogo.png" 
+            alt="Logo On Center Left" 
+            className='LogoRight'
+          />
+        </div>
 
-    <div>
-      <img 
-      src="/LcodeLogo.png" 
-      alt="Logo On Center Left" 
-      className='LogoRight'/>
-    </div>
+        <div>
+          <img 
+            src="/LcodeLogo.png" 
+            alt="Logo In Center Right" 
+            className='LogoLeft'
+          />
+        </div>
 
-    <div>
-      <img 
-      src="/LcodeLogo.png" 
-      alt="Logo In Center Right" 
-      className='LogoLeft'
-      />
-    </div>
+        <div>
+          <img 
+            src="/Screenshot_2026-03-31_124443-removebg-preview.png" 
+            alt="Pipe At Left" 
+            className='PipeLeft'
+          />
+        </div>
 
-    <div>
-      <img src='public/Pipe1' alt="" />
-    </div>
+        <div>
+          <img 
+          src="/Screenshot_2026-03-31_124452-removebg-preview.png"
+          alt="Pipe At Right"
+          className='PipeLeft'
+          id='PipeRight'
+          />
+        </div>
 
-    </center>
-</div>
+      </center>
     </>
   );
 }
