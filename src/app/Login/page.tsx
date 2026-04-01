c86df9a0628f4ed4266c9a0b31a1b7fd29cd8d38
sandro-git14 <lukavarabaloidze@gmail.com>
2026-04-01 21:42:45 +0400

    Added New Css file

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,9 +1,18 @@
+import '../styles/LoginPage.css';
+
 export default function LoginPage() {
     return (
         <>
         <center>
             <br />
-            <div className="LoginPage_Background"></div>
+            <div className='Login_Main_BG'>
+                <br />
+                <h1 className='Login_TXT'>შესვლა</h1>
+                <br /><br /><br />
+                <input type="text" className='INP_Email'/>
+                <br /><br /><br />
+                <input type="text" className='INP_Pass'/>
+            </div>
         </center>
         </>
     )
